8532d6ff736a51727c74d8e61bb7fed166f1df30
Kifemi <kifemi@outlook.com>
2020-03-24 13:56:45 +0200

    Add project files.

---
+++ MenuManagerUI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MenuManagerLibrary;
+
+namespace MenuManagerUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
