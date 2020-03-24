931946d92b3f308bb006926577fe08a6d411ca80
Kifemi <kifemi@outlook.com>
2020-03-24 14:02:31 +0200

    Minor changes

--- MenuManagerLibrary/Dish.cs
+++ MenuManagerLibrary/Dish.cs
@@ -6,5 +6,15 @@ namespace MenuManagerLibrary
 {
     class Dish
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public Dish(string name, string description, double price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+        }
     }
 }
